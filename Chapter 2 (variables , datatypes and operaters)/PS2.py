31232c58e9739beebbf304162d3db3fbe8ff386d
rixzzy11 <rizwan1242124@gmai.com>
2026-01-13 00:59:46 +0530

    DAY 2

---
+++ Chapter 2 (variables , datatypes and operaters)/PS2.py
@@ -0,0 +1,9 @@
+#to perform arithmetic operations two numbers in python
+a = 4
+b =5
+print("The additon of two numbers is:",a + b)
+print("Remainder of the two numbers is:",a % b)
+
+#to get input and get type of the data 
+c = input("Enter the value for C: ")
+print(type(c))
--- Chapter 3/PracticeSet 2.py
+++
@@ -1,5 +0,0 @@
-#how perform arithmetic operations two numbers in python
-a = 4
-b =5
-print("The additon of two numbers is:",a + b)
-print("Remainder of the two numbers is:",a % b)
